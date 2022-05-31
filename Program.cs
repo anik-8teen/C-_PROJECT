64a4323e6b0de91d45be17a90648a43f65ea8491
Mehedi Hasan Anik <70155140+anik-8teen@users.noreply.github.com>
2022-06-01 00:06:11 +0600

    task

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            int[size] arr;
+            Console.WriteLine("input numbers: ");
+            for (int i=0;i<size;i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 5; i++)
+            { 
+                Console.WriteLine (arr[i]);
+            }
+        }
+    }
+}
